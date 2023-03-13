86a23d266f0191552c3917ac9edfea4059c541c7
cjlopezortiz <106705478+cjlopezortiz@users.noreply.github.com>
2023-03-13 07:30:13 -0600

    Delete app directory

--- app/lib/common/footer.php
+++
@@ -1,5 +0,0 @@
-
-    <?php include_once 'footer_lib.php'; ?>
-</body>
-
-</html>
--- app/lib/common/general_functions.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-function toObject($arr)
-{
-    return htmlspecialchars(json_encode($arr, true), ENT_QUOTES);
-}
